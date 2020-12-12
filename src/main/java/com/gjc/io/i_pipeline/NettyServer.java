bbb01166e7c2a7c374655ccd70e934fbf84ef549
Gjc <15168264328@163.com>
2020-12-12 12:18:48 +0800

    增加pipeline

---
+++ src/main/java/com/gjc/io/i_pipeline/NettyServer.java
@@ -0,0 +1,56 @@
+package com.gjc.io.i_pipeline;
+
+import com.gjc.io.i_pipeline.inbound.InBoundHandlerA;
+import com.gjc.io.i_pipeline.inbound.InBoundHandlerB;
+import com.gjc.io.i_pipeline.inbound.InBoundHandlerC;
+import com.gjc.io.i_pipeline.outbound.OutBoundHandlerA;
+import com.gjc.io.i_pipeline.outbound.OutBoundHandlerB;
+import com.gjc.io.i_pipeline.outbound.OutBoundHandlerC;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.util.Date;
+
+public class NettyServer {
+
+    private static final int PORT = 8000;
+
+    public static void main(String[] args) {
+        NioEventLoopGroup boosGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        final ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap
+                .group(boosGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    protected void initChannel(NioSocketChannel ch) {
+                        // inBound，处理读数据的逻辑链
+                        ch.pipeline().addLast(new InBoundHandlerA());
+                        ch.pipeline().addLast(new InBoundHandlerB());
+                        ch.pipeline().addLast(new InBoundHandlerC());
+
+                        // outBound，处理写数据的逻辑链
+                        ch.pipeline().addLast(new OutBoundHandlerA());
+                        ch.pipeline().addLast(new OutBoundHandlerB());
+                        ch.pipeline().addLast(new OutBoundHandlerC());
+                    }
+                });
+
+
+        bind(serverBootstrap, PORT);
+    }
+
+    private static void bind(final ServerBootstrap serverBootstrap, final int port) {
+        serverBootstrap.bind(port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println(new Date() + ": 端口[" + port + "]绑定成功!");
+            } else {
+                System.err.println("端口[" + port + "]绑定失败!");
+            }
+        });
+    }
+}
---
+++ src/main/java/com/gjc/io/j_channel_handler/PacketDecoder.java
@@ -0,0 +1,18 @@
+package com.gjc.io.j_channel_handler;
+
+import com.gjc.io.f_communicate.PacketCodeC;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+public class PacketDecoder extends ByteToMessageDecoder {
+
+
+    @Override
+    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
+        list.add(PacketCodeC.getInstance().decode(byteBuf));
+    }
+
+}
